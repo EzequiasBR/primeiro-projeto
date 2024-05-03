075fc5690e6c4fd3046a84af09ebc570c9109b6f
EzequiasBR <ezequiashunk@gmail.com>
2024-05-02 21:43:47 -0300

    subindo arquivos do curso

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+   selector: 'app-primeiro',
+   template: `
+     <p>Meu primeiro component com angular 2!</p>
+   `
+})
+
+export class MeuPrimeiroComponent {};
